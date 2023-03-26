e1b2a9551ff4fc8c09c9f88196021e540c69c7c5
wedong.fu <fuwendong5@outlook.com>
2023-03-26 22:16:43 +0800

    first commit

---
+++ pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>aaa <span style="background-color: blue;">{{ msg }}</span></div>
+  <button @click="goBack">返回</button>
+</template>
+
+<script setup lang="ts">
+const msg = ref("message");
+const router = useRouter()
+function goBack() {
+  router.back()
+}
+</script>
+<style scoped>
+div {
+  background-color: #ccc;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        this is index .js
+    </div>
+    <NuxtLink to="/home" >home</NuxtLink>
+    <NuxtLink to="/about" >about</NuxtLink>
+</template>
